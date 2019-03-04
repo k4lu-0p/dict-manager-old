1811e945899b48a898ece6a0c25552a28d960d9a
lucas-acs <lucas.rob1@live.fr>
2019-03-04 18:22:33 +0000

    [GIT] Initialisation du projet

---
+++ assets/js/layout.js
@@ -0,0 +1,9 @@
+// Framework et Librairies
+require('../../node_modules/jquery/dist/jquery.slim.js');
+require('../../node_modules/popper.js/dist/popper.min.js');
+require('../../node_modules/bootstrap/dist/js/bootstrap.js');
+require('../../node_modules/bootstrap/dist/css/bootstrap.min.css');
+
+// Feuilles de style et script personnel
+require('../scss/main.scss');
+
