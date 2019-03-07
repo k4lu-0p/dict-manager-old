4454d03c536bfb46510dc65305d7c0c326ee9ce4
lucas-acs <lucas.rob1@live.fr>
2019-03-07 17:14:13 +0000

    [FEAT] Installation de momentJS et fullcalendar

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -3,6 +3,8 @@ require('../../node_modules/jquery/dist/jquery.slim.js');
 require('../../node_modules/popper.js/dist/popper.min.js');
 require('../../node_modules/bootstrap/dist/js/bootstrap.js');
 require('../../node_modules/bootstrap/dist/css/bootstrap.min.css');
+require('../../node_modules/moment/moment');
+require('../../node_modules/fullcalendar/dist/fullcalendar');
 
 // Feuilles de style et script personnel
 require('../scss/main.scss');
